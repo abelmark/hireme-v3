be522d2c3d8ae0392a91880f1e756eb168917e30
abelmark <mabel16113@gmail.com>
2018-03-16 19:46:50 -0400

    added keys

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,10 @@
 module.exports = {
   githubClientID: "770f448b02f3a40f8d97",
-  githubtClientSecret: "cbc6821bf1d36eabae649e1c1785335c5f09e07b"
-}
+  githubClientSecret: "cbc6821bf1d36eabae649e1c1785335c5f09e07b",
+  mongoURI: "mongodb://mark:password@ds115569.mlab.com:15569/hireme-dev",
+  cookieKey: "asd;fkask;dflasjdfasdjkfa;djs",
+  stripePublishableKey: "pk_test_ZdIjPtOFMLtukINvm0SSBQEQ",
+  stripeSecretKey: "sk_test_OS3SX8ZateE4pwXmXkvSe2Iv",
+  sendGridKey: "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",
+  redirectDomain: "http://localhost:5000"
+};
