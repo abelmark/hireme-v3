e3f5a0d74222258fcc1d53841117c908b71f02dd
abelmark <mabel16113@gmail.com>
2018-04-06 19:50:23 -0400

    removed glyphicons

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -2,7 +2,6 @@ import _ from "lodash";
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { fetchUser } from "../../actions/index";
-import { SocialIcon } from "react-social-icons";
 
 import Splash from '../Splash/Splash'
 
@@ -16,17 +15,17 @@ class Dashboard extends Component {
       "http://www.linkedin.com/in/markabelseattle"
     ];
 
-    return _.map(urls, url => {
-      console.log(url);
-      return (
-        <SocialIcon
-          className={styles.social_icon}
-          color="black"
-          style={{ height: 25, width: 25 }}
-          url={url}
-        />
-      );
-    });
+    // return _.map(urls, url => {
+    //   console.log(url);
+    //   return (
+    //     <SocialIcon
+    //       className={styles.social_icon}
+    //       color="black"
+    //       style={{ height: 25, width: 25 }}
+    //       url={url}
+    //     />
+    //   );
+    // });
   }
 
   renderDashboard() {
@@ -49,7 +48,7 @@ class Dashboard extends Component {
               <h3>{this.props.user.name}</h3>
               <h5 className={styles.lightgray}>{this.props.user.bio}</h5>
               <h5>{this.props.user.location}</h5>
-              <div>{this.renderIcons()}</div>
+              {/* <div>{this.renderIcons()}</div> */}
             </div>
           </div>
           <div className={styles.Work}>
