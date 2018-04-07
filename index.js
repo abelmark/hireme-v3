53d9a2069f77660212a09b30f2c30417baccf668
abelmark <mabel16113@gmail.com>
2018-04-06 20:01:48 -0400

    port issues

--- index.js
+++ index.js
@@ -41,5 +41,6 @@ if (process.env.NODE_ENV === "production") {
 }
 
 const PORT = (process.env.PORT || 5000);
-app.listen(PORT);
-console.log(`You are listening to the smooth sounds of port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`You are listening to the smooth sounds of port ${PORT}`)
+});
