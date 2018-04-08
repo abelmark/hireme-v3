96128c53e885855e0803601faba95673db667676
abelmark <mabel16113@gmail.com>
2018-04-08 00:04:23 -0400

    keys weren't showing

---
+++ config/prod.js
@@ -0,0 +1,14 @@
+// Production - Careful with the keys here bb
+module.exports = {
+  githubClientId: process.env.GITHUB_CLIENT_ID,
+  githubClientSecret: process.env.GITHUB_CLIENT_SECRET,
+  linkedInClientID: process.env.LINKEDIN_CLIENT_ID,
+  linkedInClientSecret: process.env.LINKEDIN_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+  mailChimpApiKey: process.env.MAILCHIMP_API_KEY,
+  mailChimpClientId: process.env.MAILCHIMP_CLIENT_ID,
+  mailChimpClientSecret: process.env.MAILCHIMP_CLIENT_SECRET,
+  mailChimpListId: process.env.MAILCHIMP_LIST_ID,
+  redirectDomain: process.env.REDIRECT_DOMAIN
+};
