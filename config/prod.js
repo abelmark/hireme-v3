e0353ab1e5221e7043458f8b3045a3f5835e1fb2
abelmark <mabel16113@gmail.com>
2018-04-14 23:25:49 -0400

    fixed key

--- config/prod.js
+++ config/prod.js
@@ -1,14 +1,14 @@
 // Production - Careful with the keys here bb
 module.exports = {
-  githubClientId: process.env.GITHUB_CLIENT_ID,
+  cookieKey: process.env.COOKIE_KEY,
+  githubClientID: process.env.GITHUB_CLIENT_ID,
   githubClientSecret: process.env.GITHUB_CLIENT_SECRET,
   linkedInClientID: process.env.LINKEDIN_CLIENT_ID,
   linkedInClientSecret: process.env.LINKEDIN_CLIENT_SECRET,
-  mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY,
   mailChimpApiKey: process.env.MAILCHIMP_API_KEY,
   mailChimpClientId: process.env.MAILCHIMP_CLIENT_ID,
   mailChimpClientSecret: process.env.MAILCHIMP_CLIENT_SECRET,
   mailChimpListId: process.env.MAILCHIMP_LIST_ID,
+  mongoURI: process.env.MONGO_URI,
   redirectDomain: process.env.REDIRECT_DOMAIN
 };
